a887c4bd99e195cb503e09a4f0f900718fa33321
Aaditya Shankar <z5588180@ad.unsw.edu.au>
2024-10-27 16:24:44 +1100

    add CLI interface for testing

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/georgechieng-sc/interns-2022/folder"
 	"github.com/gofrs/uuid"
 )
 
 func main() {
+	// Change orgID here to work with different sets of sample data.
 	orgID := uuid.FromStringOrNil(folder.DefaultOrgID)
 
 	res := folder.GetAllFolders()
@@ -24,20 +26,48 @@ func main() {
 
 	// fmt.Println(error)
 
-	// input handling for component 1
+	// input handling for component 1 & 2
 	// adapted from my work here: https://github.com/bucL/sc-interns-takehometask/blob/main/main.go
 
 	var input string
 	for input != "exit" {
-		fmt.Println("Enter a folder path")
+		fmt.Println("Type 1 for component 1 and 2 for component 2 or exit to close")
 		fmt.Scan(&input)
 		if input != "exit" {
-			folders, err := folderDriver.GetAllChildFolders(orgID, input)
-			if err != nil {
-				fmt.Printf("%s\n", err)
-			} else {
-				folder.PrettyPrint(folders)
+			switch input {
+			case "1":
+				var folderName string
+				fmt.Println("Enter folder name")
+				fmt.Scan(&folderName)
+				folderName = strings.TrimSpace(folderName)
+				folders, err := folderDriver.GetAllChildFolders(orgID, folderName)
+				if err != nil {
+					fmt.Printf("%s\n", err)
+				} else {
+					folder.PrettyPrint(folders)
+					fmt.Println("")
+				}
+			case "2":
+				var source string
+				var destination string
+				fmt.Println("Enter source folder")
+				fmt.Scan(&source)
+				source = strings.TrimSpace(source)
+				fmt.Println("Enter destination folder")
+				fmt.Scan(&destination)
+				destination = strings.TrimSpace(destination)
+
+				folders, err := folderDriver.MoveFolder(orgID, source, destination)
+				if err != nil {
+					fmt.Printf("%s\n", err)
+				} else {
+					folder.PrettyPrint(folders)
+					fmt.Println("")
+				}
+			default:
+				fmt.Println("Invalid Option Try Again")
 			}
+
 		}
 	}
 }
